58f6a69ea89a24aa68c0342c8a64bb1b601be731
juhaina1996 <juhaina.mt@rariti.io>
2022-08-13 13:11:14 +0530

    using of apply keyword

--- index1.js
+++ index1.js
@@ -3,6 +3,7 @@ function doSomething(a, b) {
  }
 doSomething(); 
 console.log(propone);
+//new
 let info = {
     name : "Juhaina",
     age : 25,
@@ -10,5 +11,32 @@ let info = {
         console.log(this.name + " is " + this.age + " years old ");  //"This" keyword refers to an object
     }
 }
-   // logs John is 31 years old
-   info.Details() 
+
+  //bind function for using function from another object
+  const student2 = {
+    name: "Jimmy ",
+    age: " 6",
+  };
+  const student3 = {
+    name: "Jani ",
+    age: " 16",
+  };
+  
+
+  let result= info.Details.bind(student3);
+  result();
+  
+  const person = {
+    fullName: function() {
+      return this.firstName + " " + this.lastName;
+    }
+  }
+  //using of apply function
+  const person1 = {
+    name:"John",
+    age: 2
+  }
+  
+let result2=info.Details.apply(person1);
+
+
