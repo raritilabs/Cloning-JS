bb1b7a685e9e00cf83d9fc0c6c003fd0cffc3eb7
Nasheeda03 <nasheedaparveen.ep@rariti.io>
2022-08-13 15:26:24 +0530

    Promises in JavaScript

---
+++ index2.js
@@ -0,0 +1,31 @@
+//promise
+
+setTimeout( () => {
+    console.log("Hey you");
+},1000);
+console.log("Parvi"); //this code will first print "Parvi" and then after 1ms "Hey you"
+//If we turn the timeout into 0sec still it first prints the word "Parvi" and then "Hey you".
+//To solve this we use "Promise".
+
+new Promise((resolve,reject) => {
+    setTimeout( () => {
+        console.log("Hey you");
+        resolve();
+    },1000);
+    
+})
+.then(() =>
+{
+    console.log("Parvi");
+}); //this code will first print "Hey you" and then after 1ms "Parvi".Even if we turn the timeout value into 0 sec, it will print as mentioned before.
+new Promise((resolve,reject) => {
+    setTimeout( () => {
+        console.log("Hey you");
+        resolve();
+    },0);
+    
+})
+.then(() =>
+{
+    console.log("Parvi");
+});
