707741eed8d00b703138153d17f08c3ea72e1922
Nasheeda03 <nasheedaparveen.ep@rariti.io>
2022-08-13 13:20:20 +0530

    DW-78: this,bind,apply in Js

--- index1.js
+++ index1.js
@@ -1,4 +1,4 @@
-//this operator
+//this operator refers to an object
 let employee = {
     name: "Parvi",
     age: 21,
@@ -10,3 +10,25 @@ let employee = {
     }
   }
   employee.details()
+
+//bind() method in Js
+
+let employee2 = {
+    name: "Junu",
+    age: 25,
+    id: 2171,
+    location: "Calicut"
+}
+let result = employee.details.bind(employee2);
+result()
+
+//apply() method in Js
+
+let employee3 = {
+    name: "Aftab",
+    age: 28,
+    id: 2172,
+    location: "Kannur"
+
+}
+let result1 = employee.details.apply(employee3);
